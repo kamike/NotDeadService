7ec78a5e0e60d4896a42817939e2e2ea10e7b524
wangtao <wangtao502@hotmail.com>
2018-01-11 21:29:45 +0800

    100px

--- app/src/main/java/com/example/administrator/myapplication/FloatWindowService.java
+++ app/src/main/java/com/example/administrator/myapplication/FloatWindowService.java
@@ -39,28 +39,27 @@ public class FloatWindowService extends Service {
         WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         final WindowManager.LayoutParams params = new WindowManager.LayoutParams();
         // 类型
-        params.type = WindowManager.LayoutParams.TYPE_TOAST;
+        params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
 //        params.type = WindowManager.LayoutParams.TYPE_APPLICATION_PANEL;
         // 设置flag
 //        int flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
 //        int flags = FLAG_NOT_FOCUSABLE | FLAG_NOT_TOUCH_MODAL;
         // 如果设置了WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE，弹出的View收不到B-ack键的事件
-        params.flags = FLAG_NOT_FOCUSABLE;
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
         // 不设置这个弹出框的透明遮罩显示为黑色
 //        params.format = PixelFormat.TRANSLUCENT;
         // FLAG_NOT_TOUCH_MODAL不阻塞事件传递到后面的窗口
         // 设置 FLAG_NOT_FOCUSABLE 悬浮窗口较小时，后面的应用图标由不可长按变为可长按
         // 不设置这个flag的话，home页的划屏会有问题
 
-        params.width = 100;
-        params.height = 100;
+        params.width = 200;
+        params.height = 150;
 
         params.gravity = Gravity.CENTER;
         Button view = new Button(this);
         view.setText("100px");
         view.setBackgroundColor(Color.RED);
         windowManager.addView(view, params);
-        _thread.start();
     }
 
     @Override
@@ -79,43 +78,5 @@ public class FloatWindowService extends Service {
 
     }
 
-    Socket socket = null;
-    private Thread _thread = new Thread() {
-        PrintWriter outs = null;
 
-        public void run() {
-          /*  try {
-                //获取控件里填写的IP地址和端口号，连接服务器
-                if ((socket == null) || (socket.isConnected() == false)) {
-                    socket = new Socket("49.4.140.67",9990);
-                }
-                //获得输入输出流
-                outs = new PrintWriter(socket.getOutputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-            while (true) {
-                if ((socket == null) || (socket.isConnected() == false)) {
-                    try {
-                        socket = new Socket("49.4.140.67", 9000);
-                        // 获得输入输出流
-                        outs = new PrintWriter(socket.getOutputStream());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-                if ((socket != null) && (socket.isConnected() == true)) {
-                    outs.println("ok");
-                    outs.flush();
-                }
-                LogUtils.i("while=======");
-            }
-
-        }
-    };
 }
