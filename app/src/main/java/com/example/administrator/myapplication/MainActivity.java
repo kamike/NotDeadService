d494eaf5b78949a56e305420c67cb93b29cc1066
wangtao <wangtao502@hotmail.com>
2018-01-13 17:55:47 +0800

    22

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -4,8 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.android.floatwindowpermission.permission.FloatWindowManager;
 import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ServiceUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.blankj.utilcode.util.Utils;
 
 public class MainActivity extends Activity {
@@ -17,7 +18,7 @@ public class MainActivity extends Activity {
         Utils.init(getApplication());
         setContentView(R.layout.activity_main);
         LogUtils.i("onCreate=======");
-//        ServiceUtils.startService(FloatWindowService.class);
+        ServiceUtils.startService(FloatWindowService.class);
 //        Intent intent = new Intent(Intent.ACTION_MAIN);
 //        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 //        intent.addCategory(Intent.CATEGORY_HOME);
@@ -27,7 +28,15 @@ public class MainActivity extends Activity {
     }
 
     private void chcckPermiss() {
-        FloatWindowManager.getInstance().applyOrShowFloatWindow(this);
+//        boolean isOpen = FloatWindowManager.getInstance().checkPermission(this);
+        boolean isOpen = SettingsCompat.canDrawOverlays(this);
+
+        if (!isOpen) {
+            ToastUtils.showLong("没有打开悬浮框权限，跳转设置" + RomUtil.getVersion() + "\n" + RomUtil.getName() + "");
+            SettingsCompat.manageDrawOverlays(this);
+        } else {
+            ToastUtils.showLong("打开了悬浮框权限！");
+        }
     }
 
 
