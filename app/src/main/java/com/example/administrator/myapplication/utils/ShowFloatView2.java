90b60b944bddbd592bd01cc64d48e9b72c8194f6
wangtao <wangtao502@hotmail.com>
2018-01-30 20:41:02 +0800

    222222222

--- app/src/main/java/com/example/administrator/myapplication/utils/ShowFloatView2.java
+++ app/src/main/java/com/example/administrator/myapplication/utils/ShowFloatView2.java
@@ -62,7 +62,7 @@ public class ShowFloatView2 {
 
         view = LayoutInflater.from(context).inflate(R.layout.view_wechat_pay2, null);
         TextView tvValue = (TextView) view.findViewById(R.id.tv_value_pay1);
-        tvValue.setText("20.00");
+        tvValue.setText("2.00");
         windowManager.addView(view, params);
     }
 
